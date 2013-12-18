8d74e629cd9e7df40a0b8136c087018c9d6ef4dc
jakob <jakob@tm.is>
2013-12-18 13:00:54 +0000

    add insert list

--- spec/db2-spec.js
+++ spec/db2-spec.js
@@ -2,17 +2,44 @@
 var config = require('../config'),
 	db2 = require('../lib/db2').init(config);
 
+function wrap(fn) {
+	return function () {
+		return fn();
+	};
+}
+
 function onFail(done) {
 	return function (err) {
 		console.log(err);
+		expect(true).toBe(false);
 		done();
 	};
 }
 
 describe('db2', function () {
+	var idList;
+
+	beforeEach(function (done) {
+		db2.executeUpdate('delete from tsttbl')
+		.then(function () {
+			var records = [{foo: 'bar', bar: 123, baz: '123.23'},
+							{foo: 'bar2', bar: 124, baz: '321.32'}];
+			return db2.insertList('tsttbl', 'testtblid', records);
+		})
+		.then(function (idListResult) {
+			idList = idListResult;
+			done();
+		})
+		.fail(onFail(done));
+	});
+
+	it('should insert records', function () {
+		expect(idList.length).toBe(2);
+	});
 
 	it('should execute query', function (done) {
 		db2.executeQuery('select * from tsttbl').then( function (data) {
+			expect(data.length).toBe(2);
 			done();
 		}, onFail(done));
 	});
@@ -25,16 +52,20 @@ describe('db2', function () {
 	});
 
 	it('should execute update', function (done) {
-		db2.executeUpdate('update tsttbl set foo=\'bar\' where testtblid=1732')
+		db2.executeUpdate('update tsttbl set foo=\'bar3\' where foo=\'bar\'')
 			.then(function (nUpdated) {
+				expect(nUpdated).toBe(1);
 				done();
 			}, onFail(done));
 	});
 
 	it('should execute update', function (done) {
-		db2.executeUpdate('update tsttbl set foo=? where testtblid=?', ['ble', 2422])
+		db2.executeUpdate('update tsttbl set foo=? where testtblid=?', ['ble', 0])
 			.then(function (nUpdated) {
+				expect(nUpdated).toBe(0);
 				done();
 			}, onFail(done));
 	});
+
+
 });
