00ff1dceb464a7de66a9fda6f7bcb04337f3db75
jakob <jakob@tm.is>
2015-06-26 14:15:00 +0000

    patch: don't block with ResultSet.next in execute

--- java/src/nodejt400/ResultStream.java
+++ java/src/nodejt400/ResultStream.java
@@ -5,80 +5,75 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 
-public class ResultStream
-{
-  private ConnectionProvider connectionProvider;
+public class ResultStream {
+	private ConnectionProvider connectionProvider;
 
-  private final Connection c;
+	private final Connection c;
 
-  private final PreparedStatement st;
+	private final PreparedStatement st;
 
-  private final ResultSet rs;
+	private final ResultSet rs;
 
-  private final int columnCount;
+	private int columnCount;
 
-  private final int bufferSize;
+	private final int bufferSize;
 
-  private boolean next;
+	private boolean next;
 
-  private String sep = "[";
+	private boolean init = true;
 
-  public ResultStream(ConnectionProvider connectionProvider, Connection c, PreparedStatement st, ResultSet rs, int bufferSize) throws Exception
-  {
-    this.connectionProvider = connectionProvider;
-    this.c = c;
-    this.st = st;
-    this.rs = rs;
-    this.bufferSize = bufferSize;
-    this.columnCount = rs.getMetaData().getColumnCount();
-    this.next = rs.next();
-  }
+	private String sep = "[";
 
-  public void close() throws Exception
-  {
-    next = false;
-    rs.close();
-    st.close();
-    connectionProvider.returnConnection(c);
-  }
+	public ResultStream(ConnectionProvider connectionProvider, Connection c,
+			PreparedStatement st, ResultSet rs, int bufferSize)
+			throws Exception {
+		this.connectionProvider = connectionProvider;
+		this.c = c;
+		this.st = st;
+		this.rs = rs;
+		this.bufferSize = bufferSize;
+	}
 
-  public String read() throws Exception
-  {
-    if (next)
-    {
-      try
-      {
-        int i = 0;
-        StringBuffer sb = new StringBuffer();
-        while (next && i < bufferSize)
-        {
-          JSONArray row = new JSONArray();
-          for (int j = 1; j <= columnCount; j++)
-          {
-            row.add(JdbcJsonClient.trim(rs.getString(j)));
-          }
-          sb.append(sep);
-          sep = ",";
-          sb.append(row.toJSONString());
-          next = rs.next();
-          i++;
-        }
-        if(!next)
-        {
-          sb.append("]");
-        }
-        return sb.toString();
-      }
-      catch (Exception ex)
-      {
-        close();
-        throw ex;
-      }
-    }
-    close();
-    return null;
-  }
+	public void close() throws Exception {
+		next = false;
+		rs.close();
+		st.close();
+		connectionProvider.returnConnection(c);
+	}
+
+	public String read() throws Exception {
+		if (init) {
+			this.columnCount = rs.getMetaData().getColumnCount();
+			this.next = rs.next();
+			this.init = false;
+		}
+		if (next) {
+			try {
+				int i = 0;
+				StringBuffer sb = new StringBuffer();
+				while (next && i < bufferSize) {
+					JSONArray row = new JSONArray();
+					for (int j = 1; j <= columnCount; j++) {
+						row.add(JdbcJsonClient.trim(rs.getString(j)));
+					}
+					sb.append(sep);
+					sep = ",";
+					sb.append(row.toJSONString());
+					next = rs.next();
+					i++;
+				}
+				if (!next) {
+					sb.append("]");
+				}
+				return sb.toString();
+			} catch (Exception ex) {
+				close();
+				throw ex;
+			}
+		}
+		close();
+		return null;
+	}
 
 }
