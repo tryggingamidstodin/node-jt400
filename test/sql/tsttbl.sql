a7902343f821362ab77fed7400b9455ed20cd461
bergur <bergur@bergur.is>
2021-04-26 15:58:42 +0000

    sql for tsttbl for #42

---
+++ test/sql/tsttbl.sql
@@ -0,0 +1,10 @@
+CREATE TABLE TSTTBL
+(
+   TESTTBLID  DECIMAL(15)     NOT NULL,
+   FOO        VARCHAR(20)     CCSID 871,
+   BAR        DECIMAL(9),
+   FRA        DATE,
+   TIMI       TIMESTAMP,
+   BAZ        DECIMAL(11,2),
+   CLOB       CLOB            CCSID 871
+);
